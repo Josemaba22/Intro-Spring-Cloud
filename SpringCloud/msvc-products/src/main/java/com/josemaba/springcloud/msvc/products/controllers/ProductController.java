ab76c6c13655d7336a48f0cd9c7884f6f1fdea4c
jose-barraza_neoris <jose.barraza@neoris.com>
2025-02-20 06:57:16 -0700

    13. Creando el controlador Rest.

---
+++ SpringCloud/msvc-products/src/main/java/com/josemaba/springcloud/msvc/products/controllers/ProductController.java
@@ -0,0 +1,39 @@
+package com.josemaba.springcloud.msvc.products.controllers;
+
+import java.lang.StackWalker.Option;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.josemaba.springcloud.msvc.products.entites.Product;
+import com.josemaba.springcloud.msvc.products.services.ProductService;
+
+@RestController
+public class ProductController {
+
+    final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> list(){
+        return ResponseEntity.ok(this.productService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable Long id){
+        Optional<Product> productOptional = productService.findById(id);
+        if(productOptional.isPresent()){
+            return ResponseEntity.ok(productOptional.orElseThrow());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    
+}
