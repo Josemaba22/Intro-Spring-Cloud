8698999c838ae60ace86c89512dc062b8c3ea35c
jose-barraza_neoris <jose.barraza@neoris.com>
2025-03-18 22:54:40 -0700

    24. Creando controlador en items.

---
+++ SpringCloud/msvc-items/src/main/java/com/josemaba/springcloud/msvc/items/controllers/ItemController.java
@@ -0,0 +1,37 @@
+package com.josemaba.springcloud.msvc.items.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.josemaba.springcloud.msvc.items.models.Item;
+import com.josemaba.springcloud.msvc.items.services.ItemService;
+
+@RestController
+public class ItemController {
+
+    private final ItemService itemService;
+
+    public ItemController(ItemService itemService){
+        this.itemService = itemService;
+    }
+
+    @GetMapping
+    public List<Item> list() {
+        return itemService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Item> details(@PathVariable Long id) {
+        Optional<Item> itemOptional = itemService.findById(id);
+        if(itemOptional.isPresent()){
+            return ResponseEntity.ok(itemOptional.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+}
