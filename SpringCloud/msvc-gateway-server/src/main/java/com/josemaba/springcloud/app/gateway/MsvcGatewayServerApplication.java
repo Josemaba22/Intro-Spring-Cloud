80965a460b80c2cd73a8e41d5fcc471ceca37819
Jose Manuel Barraza Aispuro <jose.barraza@neoris.com>
2025-06-18 11:46:25 -0700

    44. Implementando filtros globales pre y post.

--- SpringCloud/msvc-gateway-server/src/main/java/com/josemaba/springcloud/app/gateway/MsvcGatewayServerApplication.java
+++ SpringCloud/msvc-gateway-server/src/main/java/com/josemaba/springcloud/app/gateway/MsvcGatewayServerApplication.java
@@ -1,4 +1,4 @@
-package com.josemaba.springcloud.app.gateway.msvc_gateway_server;
+package com.josemaba.springcloud.app.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ SpringCloud/msvc-gateway-server/src/main/java/com/josemaba/springcloud/app/gateway/filters/SampleGlobalFilter.java
@@ -0,0 +1,27 @@
+package com.josemaba.springcloud.app.gateway.filters;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class SampleGlobalFilter implements GlobalFilter{
+
+    private final Logger logger = LoggerFactory.getLogger(SampleGlobalFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        
+        logger.info("ejectando el filtro antes del request PRE");
+
+        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+            logger.info("ejecutando filtro POST response");
+        }));
+    }
+
+}
